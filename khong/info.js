6067ee1b671cc9bcf31742cdf81a0b51d2558a69
phuoc198 <nguyenphuocanh1982003@gmail.com>
2020-01-11 18:58:18 +0700

    .

---
+++ khong/info.js
@@ -0,0 +1,10 @@
+function bigImg(x) {
+    x.style.height = "40%";
+    x.style.width = "80%";
+    x.style.transition = "0.4s"
+    }
+  
+  function normalImg(x) {
+    x.style.height = "120px";
+    x.style.width = "80%";
+    }
---
+++ khong/music.js
@@ -0,0 +1,8 @@
+function FunctionA() {
+    window.open("Project-master/giao_dien2.html", '_self')
+}
+
+function FunctionB() {
+    let win =  window.open("info.html",'_self')
+    win.focus()
+}
